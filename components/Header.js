ced30419ff264594c5a69e773642a4146ff93be8
Jonathan Eugster <jonathan.eugster@gmail.com>
2021-04-14 09:49:29 -0500

    Moved home from jonathaneugster.com repo

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,107 @@
 export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+  return <div className="Hero">
+  <div className="overlay"></div>
+  <header>
+    <div>
+      <h1 id="headerh1">Jonathan Eugster</h1>
+      <a className="profile-link" href="https://github.com/jeugster/">
+        github
+      </a>
+      {/*<a
+        className="profile-link youtube"
+        href="https://www.youtube.com/c/Redravendrone"
+      >
+        youtube
+      </a>*/}
+      <a
+        className="profile-link"
+        href="https://twitter.com/JonathanEugster"
+      >
+        twitter
+      </a>
+    </div>
+  </header>
+  <main>
+    <section className="CV">
+      <div className="card">
+        <ul>
+          <li>
+            <h2>Code</h2>
+          </li>
+          <li>
+            <i className="fab fa-html5"></i> HTML
+          </li>
+          <li>
+            <i className="fab fa-css3"></i> CSS
+          </li>
+          <li>
+            <i className="fab fa-js"></i> JavaScript
+          </li>
+        </ul>
+      </div>
+      <div className="card">
+        <ul>
+          <li>
+            <h2>Tools</h2>
+          </li>
+          <li>
+            <i className="fab fa-git"></i> Git/Hub
+          </li>
+          <li>
+            <i className="fab fa-windows"></i> VSCode
+          </li>
+          <li>
+            <i className="fab fa-docker"></i> Docker
+          </li>
+          <li>
+            <i className="fas fa-dharmachakra"></i> K8s
+          </li>
+        </ul>
+      </div>
+      <div className="card">
+        <ul>
+          <li>
+            <h2>Skills</h2>
+          </li>
+          <li>
+            <i className="fas fa-project-diagram"></i> DNS
+          </li>
+          <li>
+            <i className="fas fa-server"></i> Cloud
+          </li>
+          <li></li>
+        </ul>
+      </div>
+      <div className="card">
+        <ul>
+          <li>
+            <h2>Certs</h2>
+          </li>
+          <li>
+            <a href="https://www.freecodecamp.org/certification/fcc78f3a6da-2bd8-4da7-b6cc-31e4110324b2/responsive-web-design">
+              Responsive Web Design
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="card">
+        <ul>
+          <li>
+            <h2>Contact</h2>
+          </li>
+          <li>
+            <a href="mailto:jonathan.eugster@gmail.com">
+              jonathan.eugster@gmail.com
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  </main>
+  <footer>
+    <div className="copyright">
+      Copyright 2021 <span id="datetime"></span>
+    </div>
+  </footer>
+</div>
 }
--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Dev Portfolio - Jonathan Eugster</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="keywords" content="WebDev, Portfolio, Resume, Jonathan Eugster"/>
+        
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header/>
       </main>
 
       <Footer />
