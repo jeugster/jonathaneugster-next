2cdca2bbf3d8764db8d0298b1312af3543e62475
Jonathan Eugster <jonathan.eugster@gmail.com>
2021-04-26 15:26:57 -0400

    added viewpoint meta

--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,17 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container-fluid overlay">
-
       <Head>
         <title>Dev Portfolio - Jonathan Eugster</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="keywords" content="WebDev, Portfolio, Resume, Jonathan Eugster"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
       </Head>
 
       <main>
       <Header/>
       </main>
-      
+
       <Footer/>
 
     </div>
