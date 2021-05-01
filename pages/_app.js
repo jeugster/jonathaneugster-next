5c50d4c725abe776bbdf9f8a8511fcda5c2a4689
Jonathan Eugster <jonathan.eugster@gmail.com>
2021-05-01 16:58:47 -0500

    if process.browser

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '@styles/globals.css'
 import TagManager from 'react-gtm-module'
- 
-const tagManagerArgs = {
-    gtmId: 'GTM-PNX7XQD'
-}
- 
-TagManager.initialize(tagManagerArgs)
 
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
+}
+
+const tagManagerArgs = {
+  gtmId: 'GTM-PNX7XQD'
+}
+
+if (process.browser) {
+  TagManager.initialize(tagManagerArgs);
 }
 
 export default Application
