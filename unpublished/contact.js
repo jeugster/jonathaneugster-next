486e48a3b4112f9fe85ff50aa02514719ae9c934
Jonathan Eugster <jonathan.eugster@gmail.com>
2021-05-01 17:39:39 -0500

    moved contact to unpublished

--- unpublished/contact.js
+++ unpublished/contact.js
@@ -1,10 +1,13 @@
 import ContactForm from '@components/ContactForm'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 
 function Contact() {
     return (
       <div>
-        
+        <Header/>
         <ContactForm />
+        <Footer />
       </div>
     )
   }
