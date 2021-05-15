20f31a2ff7789d996d176dfb7501bdd0d772670c
Jonathan Eugster <jonathan.eugster@gmail.com>
2021-05-15 16:44:14 -0400

    eslint config js to json

--- .eslintrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    "extends": "eslint:recommended", "plugin:react/recommended", "plugin:prettier/recommended",
-    "rules": {"react/react-in-jsx-scope": "off"},
-    "globals": {"React": "writable" },
-}
